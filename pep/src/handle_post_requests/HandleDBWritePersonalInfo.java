ffb449941b215d53d9d39867fb91cc9758d42cf2
ArnePunk <ArnePunk@DESKTOP-D358G6T.fritz.box>
2018-12-08 15:09:07 +0100

    Personal Info Servlet

---
+++ pep/src/handle_post_requests/HandleDBWritePersonalInfo.java
@@ -0,0 +1,43 @@
+package handle_post_requests;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HandleDBWritePersonalInfo
+ */
+@WebServlet("/HandleDBWritePersonalInfo")
+public class HandleDBWritePersonalInfo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HandleDBWritePersonalInfo() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HashMap<String, String> push_into_db = new HashMap<>();
+		for (String key : request.getParameterMap().keySet())
+		{
+			push_into_db.put(key, request.getParameterMap().get(key)[0]);
+		}
+	}
+
+}
