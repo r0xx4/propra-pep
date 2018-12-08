bc3a971399f72c6536142b4a9b4d4a4fdbd5795c
ArnePunk <ArnePunk@DESKTOP-D358G6T.fritz.box>
2018-12-08 04:49:19 +0100

    Project Settings Servlet

---
+++ pep/src/handle_post_requests/HandleDBWriteProjectSettings.java
@@ -0,0 +1,42 @@
+package handle_post_requests;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HandleDBWriteProjectSettings
+ */
+@WebServlet("/handle_db_write_project_settings")
+public class HandleDBWriteProjectSettings extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HandleDBWriteProjectSettings() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HashMap<String, String> push_into_db = new HashMap<>();
+		for (String key : request.getParameterMap().keySet())
+		{
+			push_into_db.put(key, request.getParameterMap().get(key)[0]);
+		}
+	}
+}
