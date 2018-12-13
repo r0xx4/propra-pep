da34033777e4f829b3ede5d5fc63b8e94dd2a087
Jan <ja.neumann@outlook.de>
2018-12-13 23:00:40 +0100

    PDFcreator bestenliste fertig

--- pep/src/download/HandleBestRanked.java
+++ pep/src/download/HandleBestRanked.java
@@ -9,7 +9,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -49,13 +53,11 @@ public class HandleBestRanked extends HttpServlet {
 		try {
 			int anzahl = datenhaltung.getSubCat("organisationseinheit").size();
 			ArrayList<HashMap<String, String>> units = datenhaltung.getSubCat("organisationseinheit");
-			HashMap<String, ArrayList<HashMap<String, String>>> teams = new HashMap<String, ArrayList<HashMap<String, String>>>();
+			LinkedHashMap<String, ArrayList<HashMap<String, String>>> teams = new LinkedHashMap<String, ArrayList<HashMap<String, String>>>();
 			for(HashMap<String, String> u : units) {
 				teams.put(u.get("organisationseinheitname_ID"), new ArrayList<HashMap<String, String>>());
-				//System.out.println(teams);
 				for(HashMap<String, String> t : datenhaltung.getSubCat("team")) {
 					if(u.get("organisationseinheitname_ID").equals(t.get("organisationseinheitname_ID"))) {
-
 						teams.get(u.get("organisationseinheitname_ID")).add(new HashMap<String, String>(){
 							{
 								put(t.get("teamname_ID"), t.get("note"));
@@ -64,30 +66,56 @@ public class HandleBestRanked extends HttpServlet {
 					}
 				}
 			}
+			
+			for(String g : teams.keySet()) {
+				Collections.sort(teams.get(g), new Comparator<HashMap<String, String>>() {
+					@Override
+					public int compare(HashMap<String, String> a, HashMap<String, String> b) {
+						return Float.compare(
+								Float.parseFloat(a.values().iterator().next()),
+								Float.parseFloat(b.values().iterator().next()));
+					}
+				});
+			}
+			ArrayList<Integer> winnerTeams = new ArrayList<Integer>();
+			ArrayList<String> teamNames = new ArrayList<String>();
+			for(String g : teams.keySet()) {
+				int i = 0;
+				//TODO 
+				for(; i < 2 && teams.get(g).iterator().hasNext(); i++) {
+					teamNames.add(teams.get(g).get(i).keySet().iterator().next());
+				}
+				if(i != 0)
+					winnerTeams.add(i);
+			}
+			
+			
 
 			
 			System.out.println(teams);
+			System.out.println(winnerTeams);
+			System.out.println(teamNames);
+			
+			Pdfcreator pdf = new Pdfcreator();
+			pdf.creategewinnerliste(anzahl, winnerTeams, teamNames, path.toString());
+			
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			String file = "Gewinnerliste.pdf";
+			response.setContentType("APPLICATION/OCTET-STREAM");
+			response.setHeader("Content-Disposition", "attachment; filename=\""+file+"\"");
+			
+			FileInputStream fileInputStream = new FileInputStream(path+File.separator+file);
+			
+			int i;
+			while((i = fileInputStream.read()) != -1) {
+				out.write(i);
+			}
+			fileInputStream.close();
+			out.close();
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
-		
-//		Pdfcreator pdf = new Pdfcreator();
-//		//pdf.creategewinnerliste(2, list, list2, path);
-//		
-//		response.setContentType("text/html");
-//		PrintWriter out = response.getWriter();
-//		String file = "Gewinnerliste.pdf";
-//		response.setContentType("APPLICATION/OCTET-STREAM");
-//		response.setHeader("Content-Disposition", "attachment; filename=\""+file+"\"");
-//		
-//		FileInputStream fileInputStream = new FileInputStream(path+File.separator+file);
-//		
-//		int i;
-//		while((i = fileInputStream.read()) != -1) {
-//			out.write(i);
-//		}
-//		fileInputStream.close();
-//		out.close();
 	}
 
 	/**
